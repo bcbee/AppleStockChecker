8d8abf4d8bb5fa73edd780cdb78393b923131478
Brendan Boyle <brendancboyle@me.com>
2016-12-30 23:25:28 -0500

    Added project

---
+++ AppleStockChecker/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AppleStockChecker
+//
+//  Created by Brendan Boyle on 12/30/16.
+//  Copyright © 2016 Brendan Boyle. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
